bbbe6bd7f36c702dc935c1ac32b43b2cfdbbf510
prince12m <prince.malonga12@gmail.com>
2021-12-06 00:28:46 +0000

    Modelling my map in Unity because the path I modelled was doing a madness by not letting my character walk on it even though I put the same parameters that was on the original floor in unity.
    I have to figure out how to use my floor models
    
    Signed-off-by: prince12m <prince.malonga12@gmail.com>

--- 3D Game Project/Assets/StickyPlatform.cs
+++ 3D Game Project/Assets/StickyPlatform.cs
@@ -19,7 +19,7 @@ public class StickyPlatform : MonoBehaviour
             collision.transform.parent = null;
         }
     }
-
+    
     // Start is called before the first frame update
     void Start()
     {
