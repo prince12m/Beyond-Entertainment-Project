846aef56fb5aabaa03308a22a4fff805d1e214a1
prince12m <pmalonga2021@elam.co.uk>
2022-04-29 11:53:33 +0100

    nice

--- 3D Game Project/Assets/Scripts/NPC.cs
+++ 3D Game Project/Assets/Scripts/NPC.cs
@@ -3,6 +3,57 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
+[System.Serializable]
+public class NPC : MonoBehaviour {
+
+    public Transform ChatBackGround;
+    public Transform NPCCharacter;
+
+    private DialogueSystem dialogueSystem;
+
+    public string Name;
+
+    [TextArea(5, 10)]
+    public string[] sentences;
+
+    void Start () {
+        dialogueSystem = FindObjectOfType<DialogueSystem>();
+    }
+	
+	void Update () {
+          Vector3 Pos = Camera.main.WorldToScreenPoint(NPCCharacter.position);
+          Pos.y += 100;
+          ChatBackGround.position = Pos;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        this.gameObject.GetComponent<NPC>().enabled = true;
+        FindObjectOfType<DialogueSystem>().EnterRangeOfNPC();
+        if ((other.gameObject.tag == "Player") && Input.GetKeyDown(KeyCode.F))
+        {
+            this.gameObject.GetComponent<NPC>().enabled = true;
+            dialogueSystem.Names = Name;
+            dialogueSystem.dialogueLines = sentences;
+            FindObjectOfType<DialogueSystem>().NPCName();
+        }
+    }
+
+    public void OnTriggerExit()
+    {
+        FindObjectOfType<DialogueSystem>().OutOfRange();
+        this.gameObject.GetComponent<NPC>().enabled = false;
+    }
+}
+
+
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
 public class NPC : MonoBehaviour
 {
     public Transform ChatBackground;
@@ -48,4 +99,4 @@ public class NPC : MonoBehaviour
         FindObjectOfType<DialogueSystem>().OutOfRange();
         this.gameObject.getComponent<NPC>().enabled = false;
     }
-}
+}*/
